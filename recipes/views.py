49adcd082e3926bdc23bca398dee6bb90a95c31b
Ricardo <ricadomi77@gmail.com>
2023-11-16 21:17:34 -0300

    view - template

--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', context={'nome': 'Ricardo'} )
 
 def contato(request):
     return HttpResponse('PÁGINA DE CONTATO')
