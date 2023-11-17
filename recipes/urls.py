ce10088017b9def610eca0f3c9af68589414feec
Ricardo <ricadomi77@gmail.com>
2023-11-16 21:28:15 -0300

    removido contatos e sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from django.http import HttpResponse
-from recipes.views import home, contato, sobre
+from recipes.views import home
 
 
 urlpatterns = [
     path('', home),
-    path('contato/', contato),
-    path('sobre/', sobre),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html', context={'nome': 'Ricardo'} )
-
-def contato(request):
-    return HttpResponse('PÁGINA DE CONTATO')
-
-def sobre(request):
-    return HttpResponse('SOBRE')
